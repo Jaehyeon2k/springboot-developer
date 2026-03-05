07f354f9e9087f9f72f0b42730e4726b736eba56
jhkim <wmspfl2673@gmail.com>
2026-03-05 12:40:42 +0900

    init: Spring Boot project setup

---
+++ src/main/java/me/jhkim/springbootdeveloper/HelloWorldController.java
@@ -0,0 +1,29 @@
+package me.jhkim.springbootdeveloper;
+
+import me.jhkim.springbootdeveloper.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    // http://localhost/student?firstName=GilDong&lastName=Hong
+    @GetMapping("/student")
+    public Student getStudent(@RequestParam("firstName") String firstName, @RequestParam("lastName")String lastName) {
+        return new Student(firstName, lastName);
+    }
+
+    // http://localhost/student/ChanHo/Park
+    @GetMapping("/student/{firstName}/{lastName}")
+    public Student getStudent2(@PathVariable("firstName") String firstName,@PathVariable("lastName") String lastName) {
+        return new Student(firstName, lastName);
+
+    }
+
+}
